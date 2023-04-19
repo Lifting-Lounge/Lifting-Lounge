ba71d3ed2baac4ac71266e8a81e20d2ff2697f5e
Justin Jaikaran <justin.jaikaran@my.uwi.edu>
2023-04-19 05:18:14 +0000

    cli

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
-import click, pytest, sys
+import click
 from flask import Flask
 from flask.cli import with_appcontext, AppGroup
+from flask_cli import FlaskCLI
 
 from App.database import db, get_migrate
 from App.main import create_app
