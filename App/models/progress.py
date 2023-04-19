1e006afdb3f8a83b048f68e17fee755b1832e5c0
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-18 23:25:21 -0400

    Update progress.py

--- App/models/progress.py
+++ App/models/progress.py
@@ -4,4 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Progress(db.Model):
-  
+  height = db.Column(db.Integer)
+  weight = db.Column(db.Integer)
+  exercises = db.Column(db.String(120)) 
+  goals = db.Column(db.String(120))
+  routine = db.Column(db.String(120)) 
