b9ee5811e08c3ad476611f8482f5d8462ea965be
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-19 00:04:12 -0400

    Update scheduletrainer.py

--- App/models/scheduletrainer.py
+++ App/models/scheduletrainer.py
@@ -4,4 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class ScheduleTrainer(db.Model):
-  
+  username = db.Column(db.String(80), nullable = False, db.ForeignKey('user.username')) 
+  trainer = db.Column(db.String(120))
+  time = db.Column(db.DateTime)
+  booked = db.Column(db.Boolean)
