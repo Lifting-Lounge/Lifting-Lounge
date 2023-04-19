28cf8c126bd24efe7c1f597fa7f871be9d465916
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-18 23:13:32 -0400

    Update payment.py

--- App/models/payment.py
+++ App/models/payment.py
@@ -4,4 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Payment(db.Model):
+  payment_method = db.Column(db.String, nullable = False)
+  amount_paid = db.Column(db.Double)
+  expires = db.Column(db.DateTime, nullable = False)
   
