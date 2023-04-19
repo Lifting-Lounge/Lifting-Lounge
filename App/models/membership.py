6bda9eb2b7e47b40fdddc3046963a487277b4fef
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-18 23:22:22 -0400

    Update membership.py

--- App/models/membership.py
+++ App/models/membership.py
@@ -4,4 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Membership(db.Model):
-  
+  cost = db.Column(db.Double, nullable = False)
+  duration = db.Column(db.DateTime, nullable = False)
+  restrictions = db.COlumn(db.String(120))
