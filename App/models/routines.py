44adb8fdf2260feb2d3a841f07601f62fbed729e
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-18 23:15:11 -0400

    Create routines.py

---
+++ App/models/routines.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func 
+from werkzeug.security import generate_password_hash, check_password_hash
+db = SQLAlchemy()
+
+class Routines(db.Model):
+  
