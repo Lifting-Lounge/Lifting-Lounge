567848ff9099eaebf031255e54a0cc2cd64a14cb
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-18 23:28:23 -0400

    Update routines.py

--- App/models/routines.py
+++ App/models/routines.py
@@ -4,4 +4,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Routines(db.Model):
+  workout_name = db.Column(db.String(120), nullable = False)
+  reps = db.Column(db.Integer) 
+  sets = db.Column(db.Integer) 
+  routine_name = db.Column(db.String(120))
+  day = db.Column(db.String(120)) 
   
