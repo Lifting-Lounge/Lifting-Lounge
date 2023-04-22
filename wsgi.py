5c955485ab908fa53ea64b7aed236f41277e0e14
Justin Jaikaran <justin.jaikaran@my.uwi.edu>
2023-04-22 18:12:03 +0000

    neaten

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import click
 from flask import Flask
 from flask.cli import with_appcontext, AppGroup
-from flask_cli import FlaskCLI
 
 from App.database import db, get_migrate
 from App.main import create_app
