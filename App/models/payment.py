0db284af27957af869eb9b30199d545c02615bfa
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-22 00:49:36 -0400

    Update payment.py

--- App/models/payment.py
+++ App/models/payment.py
@@ -8,3 +8,10 @@ class Payment(db.Model):
   amount_paid = db.Column(db.Double)
   expires = db.Column(db.DateTime, nullable = False)
   
+  
+  def __init__(self, payment_method, amount_paid, expires):
+    self.payment_method = payment_method
+    self.amount_paid = amount_paid
+    self.expires = expires
+    
+   
