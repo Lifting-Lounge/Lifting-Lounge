1a18eed04e62fbbee6a0a3b7b0e08934915c0e2a
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-22 00:40:57 -0400

    Update equpiment.py

--- App/models/equpiment.py
+++ App/models/equpiment.py
@@ -7,3 +7,19 @@ class Equipment(db.Model):
   category = db.Column(db.String(120), nullable = False)
   name = db.Column(db.String(120), unique = True, nullable = False)
   has_exercises = db.Column(db.Boolean, nullable= False)
+  exercises = db.Column(db.String) 
+
+  def __init__(self, name, category):
+    self.name = name
+    self.category = category
+    
+    
+  get_json(self):
+    return{
+      'name': name
+      'category': category
+      
+      
+  def exercises(has_exercises): 
+      if(has_exercises): 
+        return exercises
