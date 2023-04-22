19dd383609a4eea1c296e7ad90bc04543e622650
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-04-22 00:54:06 -0400

    Update progress.py

--- App/models/progress.py
+++ App/models/progress.py
@@ -9,3 +9,23 @@ class Progress(db.Model):
   exercises = db.Column(db.String(120)) 
   goals = db.Column(db.String(120))
   routine = db.Column(db.String(120)) 
+  
+  def __init__(self, height, weight,exercises, goals, routine): 
+    self.height = height
+    self.weight = weight
+    self.exercises= exercises
+    self.goals = goals
+    self.routine = routine
+    
+  
+  def get_json(self);
+    return{
+      'height': height
+      'weight': weight
+      'exercises': exercises
+      'goals': goals
+      'routine': routine
+    }
+  
+  def new_measurements(): 
+    
