6f4c1aa4f0bee09542ccec873b5d1c9546533fff
Justin Jaikaran <justin.jaikaran@my.uwi.edu>
2023-04-22 20:10:22 +0000

    logintest

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -2,6 +2,18 @@ from flask_login import login_user, login_manager, logout_user, LoginManager
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 from App.models import *
 
+
+def jwt_authenticate(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user and user.check_password(password):
+    return create_access_token(identity=username)
+  return None
+
+# def login(username, password):
+#     user = User.query.filter_by(username=username).first()
+#     if user and user.check_password(password):
+#         return user
+#     return None
 def login(username, password):
     # user = User.query.filter_by(username=username).first()
     # if user and user.check_password(password):
@@ -10,7 +22,7 @@ def login(username, password):
         return user
     
     return None
-
+    
 def setup_flask_login(app):
     login_manager = LoginManager()
     login_manager.init_app(app)
@@ -20,3 +32,20 @@ def setup_flask_login(app):
         return User.query.get(user_id)
     
     return login_manager
+
+def setup_jwt(app):
+    jwt = JWTManager(app)
+
+    @jwt.user_identity_loader
+    def user_identity_lookup(identity):
+        user = User.query.filter_by(username=identity).one_or_none()
+        if user:
+            return user.id
+        return None
+
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.get(identity)
+
+    return jwt
