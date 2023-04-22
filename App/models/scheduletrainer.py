25d187cee9219c85a747142952220497fb02622e
Justin Jaikaran <justin.jaikaran@my.uwi.edu>
2023-04-22 17:58:45 +0000

    comment

--- App/models/scheduletrainer.py
+++ App/models/scheduletrainer.py
@@ -9,13 +9,13 @@ class ScheduleTrainer(db.Model):
   time = db.Column(db.DateTime)
   booked = db.Column(db.Boolean)
 
-def __init__(self,username, trainer, time, booked):
-    self.username = username
-    self.trainer= trainer
-    self.time = time
-    self.booked =booked
+# def __init__(self,username, trainer, time, booked):
+#     self.username = username
+#     self.trainer= trainer
+#     self.time = time
+#     self.booked =booked
     
-def book_appt(trainer, booked): 
-  if(!booked): 
-    booked = True
-    #idk here either sorry
+# def book_appt(trainer, booked): 
+#   if(!booked): 
+#     booked = True
+#     #idk here either sorry
