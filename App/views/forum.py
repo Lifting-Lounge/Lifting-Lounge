52b51bae640263320bbd19ab82f2e0270d7d48ae
Justin Jaikaran <justin.jaikaran@my.uwi.edu>
2023-04-24 04:44:07 +0000

    forums cli

--- App/views/forum.py
+++ App/views/forum.py
@@ -6,4 +6,4 @@ forum_views = Blueprint('forum_views', __name__, template_folder='../templates')
 @forum_views.route('/forum', methods=['GET'])
 def profile_page():
     # message = get_all_messages()
-    return render_template('forum.html', message=message)
+    return render_template('forum.html')
