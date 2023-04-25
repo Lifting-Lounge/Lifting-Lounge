a70fce51736701563d6abb8cd9f3094f7ab7216a
JustinThe2nd J <jjaikaran5+github@gmail.com>
2023-04-25 02:21:56 +0000

    muscle_page

--- App/views/exercises.py
+++ App/views/exercises.py
@@ -5,11 +5,20 @@ from App.models import Exercises
 
 exercise_views = Blueprint('exercise_views', __name__, template_folder='../templates')
 
+# @exercise_views.route('/exercises_page', methods=['GET','POST'])
+# def muscle_page():
+#     exercises = get_all_exercises()
+#     if exercises:
+#         return render_template('index.html', exercises=exercises)
+#     flash ('Unknown muscle group! Try biceps or quadriceps')
+#     return redirect ('/')
+
+
 @exercise_views.route('/exercises', methods=['GET','POST'])
 def muscle_action():
     muscle = request.form 
-
     exercises = load_api_muscle(muscle)
-    
-    return render_template('index.html', exercises=exercises)
-    
+    if exercises:
+        return render_template('index.html', exercises=exercises)
+    flash ('Unknown muscle group! Try biceps or quadriceps')
+    return redirect ('/')
