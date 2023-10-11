72d33f1b4d72554c2ffd59f046a2ada15aef5a20
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2023-10-10 23:21:15 -0400

    Update auth.py

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import login_required, login_user, current_user, logout_user
 
 from.index import index_views
 
 from App.controllers import (
     create_user,
+    jwt_authenticate,
     login 
 )
 
@@ -14,44 +16,71 @@ auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
 Page/Action Routes
 '''
 
-@auth_views.route('/login_page', methods=['GET'])
-def login_page():
-    return render_template('login.html')
+@auth_views.route('/users', methods=['GET'])
+def get_user_page():
+    users = get_all_users()
+    return render_template('users.html', users=users)
 
-@auth_views.route('/login', methods = ['POST'])
+
+@auth_views.route('/identify', methods=['GET'])
+@login_required
+def identify_page():
+    return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
+
+
+@auth_views.route('/login', methods=['POST'])
 def login_action():
     data = request.form
-    username = data['username']
-    password = data['password']
-    user = login(username, password)
+    user = login(data['username'], data['password'])
     if user:
         login_user(user)
-        return redirect('/home')
-    flash ('Incorrect username or password given!')
-    return redirect('/login_page')
+        return 'user logged in!'
+    return 'bad username or password given', 401
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
-    logout_user()
-    return redirect('/home')
-
-@auth_views.route('/signup_page', methods=['GET'])
-def signup_page():
-    return render_template('signup.html')
+    data = request.form
+    user = login(data['username'], data['password'])
+    return 'logged out!'
 
 @auth_views.route('/signup', methods = ['POST'])
 def signup_action():
     data = request.form
     username = data['username']
     password = data['password']
-    email = data['email']
-    payment = data['payment']
-    user = create_user(username, password, email, payment)
+    user = create_user(username, password)
     if user:
         login_user(user)
-        return redirect('/home')
+        return redirect('/')
     else:
         flash ('Username or email already in use!')
-        return redirect('/signup_page')
+        return redirect('/')
     
 
+'''
+API Routes
+'''
+
+@auth_views.route('/api/users', methods=['GET'])
+def get_users_action():
+    users = get_all_users_json()
+    return jsonify(users)
+
+@auth_views.route('/api/users', methods=['POST'])
+def create_user_endpoint():
+    data = request.json
+    create_user(data['username'], data['password'])
+    return jsonify({'message': f"user {data['username']} created"})
+
+@auth_views.route('/api/login', methods=['POST'])
+def user_login_api():
+  data = request.json
+  token = jwt_authenticate(data['username'], data['password'])
+  if not token:
+    return jsonify(message='bad username or password given'), 401
+  return jsonify(access_token=token)
+
+@auth_views.route('/api/identify', methods=['GET'])
+@jwt_required()
+def identify_user_action():
+    return jsonify({'message': f"username: {jwt_current_user.username}, id : {jwt_current_user.id}"})
